95dcc5941fb5704e4d7e6cbfee5636afee18cdbd
admica <almestaadmica@gmail.com>
2023-05-02 04:04:22 +0300

    MY CUSTOM STARTER

--- app.js
+++ app.js
@@ -1,29 +1,41 @@
-require("dotenv").config();
-require("express-async-errors");
-
-const express = require("express");
-const app = express();
-
-const notFoundMiddleware = require("./middleware/not-found");
-const errorHandlerMiddleware = require("./middleware/error-handler");
-
-// middleware
-app.use(express.static("./public"));
-app.use(express.json());
-
-app.use(notFoundMiddleware);
-app.use(errorHandlerMiddleware);
-
-const port = process.env.PORT || 3000;
-
-const start = async () => {
-  try {
-    app.listen(port, () =>
-      console.log(`Server is listening on port ${port}...`)
-    );
-  } catch (error) {
-    console.log(error);
+import express from "express";
+import connector from "./database/connector.js";
+import RouteServiceProvider from "./providers/RouteServiceProvider.js";
+import notFound from "./app/middleware/not-found.js";
+import errorHandlerMiddleware from "./app/middleware/error-handler.js";
+import expressAsycErrors from "express-async-errors";
+class App {
+  constructor() {
+    this.server = express();
+    this.middlewares();
+    this.routes();
+    this.server.use(notFound);
+    this.server.use(errorHandlerMiddleware);
   }
-};
 
-start();
+  middlewares() {
+    this.server.use(express.static("public"));
+    this.server.use(express.json());
+  }
+
+  routes() {
+    new RouteServiceProvider(this.server);
+  }
+}
+
+var server = new App().server;
+
+var port = process.env.PORT || 3000;
+
+connector
+  .mongo()
+  .then((result) => {
+    server.listen(port, () => {
+      console.log(
+        `Started Express server at port ${port}... \n you can run it clicking this link http://localhost:${port}`
+      );
+    });
+  })
+  .catch((err) => {
+    console.error(`Failed to connect to DB ${err}`);
+  });
